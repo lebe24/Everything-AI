177243b8b8ccd5b11a4d15e4a8a68ae91cb76f3b
Emmanuel Philip <31292486+lebe24@users.noreply.github.com>
2024-06-25 23:44:33 +0000

    ..

---
+++ python/demo.py
@@ -0,0 +1,34 @@
+"""
+Install the Google AI Python SDK
+
+$ pip install google-generativeai
+
+See the getting started guide for more information:
+https://ai.google.dev/gemini-api/docs/get-started/python
+"""
+
+# import os
+
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyBnPaVUw2dzpfMgxRyJbX5hDS7fs1aOEpc")
+
+# Create the model
+# See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
+
+response = model.generate_content("Write a story about a AI and magic")
+print(response.text)
